50a5a301d944e53fdd6ea9c42759aef0912af104
runlevel4 <90661907+runlevel-4@users.noreply.github.com>
2026-07-14 17:35:42 -0400

    Docker for LMDE
    This will install docker for Linux Mint Debian Edition due to "gigi" not being an official Linux build in the docker URLs.

---
+++ docker_install_LMDE.sh
@@ -0,0 +1,29 @@
+# remove old docker packages
+sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-doc podman-docker containerd runc | cut -f1)
+
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: trixie
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+# install latest docker version
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+#start docker service
+sudo systemctl start docker
+
+# test docker
+sudo docker run hello-world
