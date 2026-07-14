eb2dedbb6a697c2a8d133315d6a4d4201450738c
runlevel4 <90661907+runlevel-4@users.noreply.github.com>
2026-07-14 17:23:23 -0400

    Update docker_install.sh
    suppressed install confirmation.

--- docker_install.sh
+++ docker_install.sh
@@ -20,7 +20,7 @@ EOF
 
 # install latest docker version
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 #start docker service
 sudo systemctl start docker
